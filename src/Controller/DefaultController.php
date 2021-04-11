a2c4cb9fa2ed113b40cda16e62d51ebfc18f372f
Samir Massoundi <samir.j7.massoundi@gmail.com>
2021-04-11 16:51:43 +0200

    pagination done

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Repository\ArticleRepository;
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +15,16 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(Request $request, ArticleRepository $articleRepository, PaginatorInterface $paginator): Response
     {
+        $data = $articleRepository->findBy([], ['createdAt' => 'desc']);
+        $articles = $paginator->paginate(
+            $data,
+            $request->query->getInt('page',1),
+            5
+        );
         return $this->render('base.html.twig', [
-            'articles'=>$articleRepository->findAll([], ['createdAt' => 'desc']),
+            'articles'=> $articles,
             ]);
     }
 }

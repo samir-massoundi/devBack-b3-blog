db53763ad113904bc411ecbf7ed6d6f65e052362
Samir Massoundi <samir.j7.massoundi@gmail.com>
2021-04-19 11:50:43 +0200

    Added new article form on the admin dashboard page

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
 use App\Repository\CommentaireRepository;
+use DateTime;
 use Doctrine\Common\Collections\Criteria;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,23 +14,25 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Message;
 
 /** 
  * @Route("admin/dashboard")
  * @IsGranted("ROLE_ADMIN") 
  * 
-*/
+ */
 class AdminDashboardController extends AbstractController
 {
     /**
      * @Route("/", name="admin_dashboard")
      */
-    public function index(  ArticleRepository $articleRepository,
-                            CommentaireRepository $commentaireRepository): Response
-    {
-        $lastArticles = $articleRepository->findBy(['isVisible'=> 1], ['createdAt' => 'desc'], 5);
+    public function index(
+        ArticleRepository $articleRepository,
+        CommentaireRepository $commentaireRepository
+    ): Response {
+        $lastArticles = $articleRepository->findBy(['isVisible' => 1], ['createdAt' => 'desc'], 5);
 
-        $nbCommentsToReview = $commentaireRepository->count(['state'=> '0']);
+        $nbCommentsToReview = $commentaireRepository->count(['state' => '0']);
 
         $user = $this->getUser();
 
@@ -41,57 +45,93 @@ class AdminDashboardController extends AbstractController
     /**
      * @Route("/mes-articles", name="my-articles", methods={"GET"})
      */
-    public function myArticles( Request $request,
-                                CommentaireRepository $commentaireRepository,
-                                ArticleRepository $articleRepository,
-                                PaginatorInterface $paginator) : Response
-    {   
-        $nbCommentsToReview = $commentaireRepository->count(['state'=> '0']);
-    
-        $data=$articleRepository->findBy(['auteur' => $this->getUser()->getId() ], ['createdAt'=> 'desc']);
+    public function myArticles(
+        Request $request,
+        CommentaireRepository $commentaireRepository,
+        ArticleRepository $articleRepository,
+        PaginatorInterface $paginator
+    ): Response {
+        $nbCommentsToReview = $commentaireRepository->count(['state' => '0']);
+
+        $data = $articleRepository->findBy(['auteur' => $this->getUser()->getId()], ['createdAt' => 'desc']);
         $myArticles = $paginator->paginate(
             $data,
             $request->query->getInt('page', 1),
             5
         );
 
-        return $this->render('admin_dashboard/my-articles.html.twig',
-        [
-            'myArticles' => $myArticles,
-            'nbCommentsToReview' => $nbCommentsToReview,
-        ]);
+        return $this->render(
+            'admin_dashboard/my-articles.html.twig',
+            [
+                'myArticles' => $myArticles,
+                'nbCommentsToReview' => $nbCommentsToReview,
+            ]
+        );
     }
 
     /**
      * @Route("/gestion-commentaire", name="commentReview", methods={"GET", "POST"})
      */
-    public function reviewComments( Request $request,
-                                    CommentaireRepository $commentaireRepository,
-                                    PaginatorInterface $paginator
-                                    ): Response
-    {   
+    public function reviewComments(
+        Request $request,
+        CommentaireRepository $commentaireRepository,
+        PaginatorInterface $paginator
+    ): Response {
         $data = $commentaireRepository->findBy([]);
-        
-        $nbCommentsToReview = $commentaireRepository->count(['state'=> '0']);
+
+        $nbCommentsToReview = $commentaireRepository->count(['state' => '0']);
         $comments = $paginator->paginate(
             $data,
-            $request->query->getInt('page',1),
+            $request->query->getInt('page', 1),
             10
         );
-        return $this->render('admin_dashboard/comment-review.html.twig',
-        [
-            'comments' => $comments,
-            'nbCommentsToReview' => $nbCommentsToReview
-        ]);
+        return $this->render(
+            'admin_dashboard/comment-review.html.twig',
+            [
+                'comments' => $comments,
+                'nbCommentsToReview' => $nbCommentsToReview
+            ]
+        );
     }
 
+    /**
+     * @Route("/rediger-article", name="article-create", methods={"GET", "POST"})
+     */
+    public function createArticle(
+        Request $request, 
+        CommentaireRepository $commentaireRepository) : Response
+    {
+        $nbCommentsToReview = $commentaireRepository->count(['state' => '0']);
+
+        $article = new Article();
+        $form = $this->createForm(ArticleFormType::class,$article);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $article->setcreatedAt(new DateTime());
+            $article->setAuteur($this->getUser());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+            return $this->redirectToRoute('my-articles', [
+                'message' => 'Votre Article à été ajoutés dans votre liste'
+            ]);
+
+        }
+        return $this->render('admin_dashboard/article.html.twig',
+        [   
+            'form' => $form->createView(), 
+            'nbCommentsToReview' => $nbCommentsToReview,
+            'title' => 'Rédiger un nouvel article'
+        ]);
+    }
 
     /**
      * @Route("/{id}", name="article_delete", methods={"DELETE"})
      */
     public function delete(Request $request, Article $article): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($article);
             $entityManager->flush();
--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Categorie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +17,21 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('headline')
-            ->add('subheadline')
-            ->add('articleContent')
-            ->add('createdAt')
+            ->add('headline', TextType::class)
+            ->add('subheadline', TextType::class)
+            ->add('articleContent', TextAreaType::class)
+            // ->add('createdAt')
             ->add('imageArticle')
-            ->add('auteur')
-            ->add('categorie')
+            ->add('categorie', EntityType::class,
+            [
+                'class' => Categorie::class,
+                'choice_label'=> function($category){
+                    return $category->getName();
+                },
+                'expanded' => false,
+                'multiple' => false
+            ])
+            ->add('isVisible', CheckboxType::class)
         ;
     }
 

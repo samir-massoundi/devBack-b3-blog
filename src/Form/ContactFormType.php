f45f095ce3d001c5f81dbbc8bb24f11d68f8b784
Samir Massoundi <samir.j7.massoundi@gmail.com>
2021-04-19 11:47:55 +0200

    minor changes

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,12 +15,17 @@ class ContactFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lastname', TextType::class)
             ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
             ->add('mailContact', EmailType::class)
             ->add('Telephone', TextType::class)
-            ->add('messageContact', TextType::class)
-        ;
+            ->add('messageContact', TextareaType::class, 
+            [
+                'attr' =>
+                [
+                    'class' => 'form-control'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

f9aec01fe2847565b694c24897e9fc4cd2c72c40
Samir Massoundi <samir.j7.massoundi@gmail.com>
2021-04-20 11:55:50 +0200

    Contact fixtures. final commit

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -6,8 +6,6 @@ use Faker\Factory;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\Security\Core\User\User as UserUser;
-use Symfony\Component\Console\CommandLoader\FactoryCommandLoader;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UsersFixtures extends Fixture
